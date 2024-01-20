0a9d78763d22747a9e733a18cd313e7e32ac2812
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-20 16:55:26 -0300

    preparing for hole

--- src/main/java/com/example/demo/controllers/MainControllers.java
+++ src/main/java/com/example/demo/controllers/MainControllers.java
@@ -1,14 +1,36 @@
 package com.example.demo.controllers;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Flux;
+
+/**
+ * 
+ * Podriamos armar todo el camino: controller -> service -> repository manejandonos con Mono/Flux
+ * 
+ * Al retornar un Flux, vemos que no hacemos .subscribe(), eso porque se hace automáticamente.
+ * 
+ * Lo interesante es que, si todo esta armado como dije más arriba, al entrar al controller, se retorna 
+ * el Flux de forma automática, aunque todavía ni se hubiera pegado a la base. Cuando se obtenga la data
+ * se enviará al Flux y se recibirá, ahi esta el potencial!
+ * 
+ */
 @RestController
 public class MainControllers {
 
 	@GetMapping("/api/webflux")
-	public String mainController() {
-		return "WebFlux!!!";
+	public Flux<String> mainController() {
+		
+		String[] fruits = new String[] {"Apple", "Orange", "Grape", "Banana"};
+		
+		return Flux.fromArray(fruits);
+		
+		
 	}
+	
+	
+	
 	
 }
