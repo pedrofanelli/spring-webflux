17fac2e945402472769a83ea8f0bfc50adf8121a
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-20 17:07:37 -0300

    fruit model

--- src/main/java/com/example/demo/Models/Fruit.java
+++ src/main/java/com/example/demo/Models/Fruit.java
@@ -1,6 +1,42 @@
 package com.example.demo.Models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "fruits")
 public class Fruit {
 
+	@Id
+    @Column(name = "fruit_id", nullable = false)
+    Long id;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @Column(name = "contact_name", nullable = true)
+    String description;
+
+
+    public Long getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+    	return description;
+    }
+    public void setDescription(String description) {
+    	this.description = description;
+    }
+	
 }
--- src/main/java/com/example/demo/repositories/MainRepository.java
+++ src/main/java/com/example/demo/repositories/MainRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.repositories;
 
-public class MainRepository {
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MainRepository {
 
 }
